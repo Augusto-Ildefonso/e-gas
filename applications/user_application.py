8cc7a8b0a67a64a8f0627fe31938bfa185f3b754
Augusto <afildefonso16@gmail.com>
2022-03-22 19:03:32 -0300

    Update user_application.py

--- applications/user_application.py
+++ applications/user_application.py
@@ -1,91 +1,102 @@
 import serial as s
 import time
-import pickle
+import json
 import socket
+import os
 
 device = ''  # Device's name
 received_data = ''  # Received data
 num_serie = 0  # Device's serial number
-pressao = -1  # Gas cylinder pressure
+pressure = -1  # Gas cylinder pressure
 dados = {}  # Dictionary in which the received data will be stored
 data = bytes()  # Received data from the server
+json_file = ''
 HOST = "localhost"  # The server's hostname or IP address
 PORT = 40000  # The server's port
 
+
 while True:
-    print('\nConectando à porta COM3...')
-    try:
-        # Try to connect with the serial port
-        ser1 = s.serial_for_url('COM3', 9600, timeout=10)
+    # Try to connect with the serial port
+    ser1 = s.serial_for_url('COM3', 9600, timeout=10)
+    print('Conectando à porta COM3...')
+    if ser1.is_open:
+        print('Conectada.\n')
+        # Assigns the device's name to the variable
+        device = ser1.name
 
-        if ser1.is_open:
-            print('Conectada.\n')
+        # Read the serial data
+        print('\nRecebendo os dados...')
+        received_data = ser1.read(41)  # ID uses 33 bits, ' \r\n' uses 3 bits and the pressure uses 5 bits
 
-            # Assigns the device's name to the variable
-            device = ser1.name
+        # Data processing
+        print(f'Dados recebidos: {received_data}')
+        print('\nTratando os dados...')
+        received_data = received_data.decode('utf-8')
+        received_data = received_data.split(' \r\n')
 
-            print('\nRecebendo os dados...')
+        # Devices Serial Number Extraction
+        num_serie = received_data[0]
+        num_serie = num_serie.split(': ')
+        num_serie = num_serie[1]
+        print('Tratamento concluído.')
+        print(f'\n\nATENÇÃO! SEGUE O NÚMERO DE SÉRIE DO SEU DISPOSITIVO PARA QUE SEJA ADICIONADO NO DASHBOARD.'
+              f'\nDispositivo: {device}\nNúmero de Série: {num_serie}')
 
-            # Read the serial data
-            received_data = ser1.read_until('UniqueID: 39 39 31 34 31 05 2D 17/55555', 42)
+        # Pressure Extraction
+        pressure = float(received_data[1])
 
-            print('\nTratando os dados...')
+        # Create the dictionary base for the pickling
+        dados = {
 
-            # Data processing
-            received_data = received_data.decode('utf-8')
-            received_data = received_data.split("/")
+            "ID": num_serie,
 
-            # Devices Serial Number Extraction
-            num_serie = received_data[0]
-            num_serie = num_serie.split(":")
-            num_serie = num_serie[1].split("\r")
+            "Dispositivo": device,
 
-            # Pressure Extraction
-            pressao = float(data_received[1])
+            "Pressao": pressure
 
-            # Create the dictionary base for the pickling
-            dados = {
+        }
 
-                "ID": num_serie[0],
+        # Try to serialize the JSON
+        print('\n\nCriando o arquivo JSON...')
+        try:
+            json_file = json.dumps(dados, indent=4)
+            with open('dados.json', 'w') as o:
+                o.write(json_file)
+                o.close()
+            print('Arquivo criado.')
+        except:
+            print('Não foi possível criar o arquivo JSON.')
 
-                "Dispositivo": device,
-
-                "Dados:": pressao
-
-            }
-
-            # Pickling
-            print('Serializando...)
+        # Connects with the server
+        print('\nConectando com o servidor...')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        sock.sendall(b'True')  # Send a message to check the connection with the server
+        data = sock.recv(1024)  # Receive the server response
+        data.decode('utf-8')  # Decode the server response
+        data = bool(data)  # Convert the server responde to boolean
+        if data:
+            print('Conectado.')
+            # Sending the json object
+            print('\nTentando enviar o arquivo JSON...')
             try:
-                file = pickle.dumps(dados)
-                print('Serialização completa.')
-                """
-                Tentar enviar como json, só que tem que converter o json depois para bytes
-                """
-                  
+                sock.sendall(bytes(json_file, encoding='utf-8'))
+                print('Arquivo JSON enviado.')
             except:
-                  print('Não foi possível serializar o dicionário.')
+                print('Não foi possível enviar o arquivo JSON.')
+        else:
+            print('Não foi possível estabelecer a conexão com o servidor.')
+        sock.close()
 
-            # Connects with the server
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((HOST, PORT))
-                s.sendall(b'True')
-                data = s.recv(1024)
-                data.decode('utf-8')
-                data = bool(data)
-                if data:
-                    print('Conectado.')
-                    s.sendall(file)
-                    s.close()
-                else:
-                    print('Não foi possível estabelecer a conexão com o servidor.')
+        # Disconnect with the serial port
+        print('\nDesconectando...')
+        ser1.close()
+        print('Desconectado.\n\n')
 
-            # Disconnect with the serial port
-            print('\nDesconectando...')
-            ser1.close()
-            print('Desconectado.\n\n')
+        # Waiting time until the next reading
+        time.sleep(10)
 
-            # Waiting time until the next reading
-            time.sleep(30)
-    except FileNotFoundError:
+        # Clear the terminal
+        os.system('cls')
+    else:
         print('Não foi possível estabelecer a conexão.')
